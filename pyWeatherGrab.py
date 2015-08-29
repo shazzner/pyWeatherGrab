ce31112e1d7088b2f28f46ef3b57c4e040e3fd71
Chris Hardee <shazzner@gmail.com>
2015-08-29 14:55:44 -0500

    Added in some commandline checks, help first

--- pyWeatherGrab.py
+++ pyWeatherGrab.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python
 # Read weather data from serial input such as an Arduino
+
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+def help():
+    help_msg = 'pyWeatherGrab - grab weather sensor data via serial\n'\
+    '\n'\
+    '-h                                 Show this help\n'\
+    '-s, --serial-port <Serial Port>    Set the serial port: ie /dev/ttyUSB0\n'\
+    '-b, --baud <Baud Rate>             Set the baud rate\n'
+
+    return help_msg
+
+#while True:
+#    print(ser.readline())
+
+def parse_options():    
+    try:
+        long_options = ['serial-port', 'baud', 'help']
+        opts, args = getopt.getopt(sys.argv[1:], "shp:w:", long_options)
+    except getopt.GetoptError, e:
+        print help()
+        sys.exit(3)
+    
+if __name__ == '__main__':
+    baud, stream = parse_options()
+
+    baud = 9600
+    
